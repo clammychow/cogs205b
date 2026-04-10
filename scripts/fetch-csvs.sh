3c675380ec3466a1d94d391d76a323fdf5f6ebba
Anusha <anushs5@uci.edu>
2026-04-10 08:51:35 +0000

    add step to download and unzip data

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# creates temporary directory
+mkdir -p /tmp/extract
+
+# downloads and unzips data to temp
+echo "unzipping files"
+wget -0 data.zip https://github.com/joachimvandekerckhove/cogs205b-s26/raw/9dca64e57fd88213f2422c19a8b10953a8fbfdbe/modules/02-version-control/files/data.zip
+unzip -q data.zip -d /tmp/extract
