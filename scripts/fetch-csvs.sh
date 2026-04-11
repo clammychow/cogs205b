271dfd14e6aca78ff33cc5fc7ddb195f37c3ee8e
Anusha <anushs5@uci.edu>
2026-04-11 05:21:08 +0000

    add step to extract csvs to data folder

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+set -e
 
-# creates temporary directory
+# creates temp and target directories
 mkdir -p /tmp/extract
+curr_date=$(date +%Y-%m-%d)
+mkdir -p ./data/$curr_date
 
 # downloads and unzips data to temp
 echo "unzipping files"
-wget -0 data.zip https://github.com/joachimvandekerckhove/cogs205b-s26/raw/9dca64e57fd88213f2422c19a8b10953a8fbfdbe/modules/02-version-control/files/data.zip
+wget -O data.zip "https://raw.githubusercontent.com/joachimvandekerckhove/cogs205b-s26/main/modules/02-version-control/files/data.zip"
 unzip -q data.zip -d /tmp/extract
+
+# filters and moves files with .csv endings
+echo "extracting csvs"
+for file in /tmp/extract/*.csv; do
+    mv "$file" "./data/$curr_date"
+done
