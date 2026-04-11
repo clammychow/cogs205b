3e9f2206ff509b4aaace1b2f5c0da9500ea40dc3
Anusha <anushs5@uci.edu>
2026-04-11 07:32:31 +0000

    change how temporary directory is created

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -2,22 +2,22 @@
 set -e
 
 # creates temp and target directories
-mkdir -p /tmp/extract
+temp_dir=$(mktemp -d)
 curr_date=$(date +%Y-%m-%d)
 mkdir -p ./data/$curr_date
 
 # downloads and unzips data to temp
 echo "unzipping files"
 wget -O data.zip "https://raw.githubusercontent.com/joachimvandekerckhove/cogs205b-s26/main/modules/02-version-control/files/data.zip"
-unzip -q data.zip -d /tmp/extract
+unzip -q data.zip -d "$temp_dir"
 
 # filters and moves files with .csv endings
 echo "extracting csvs"
-for file in /tmp/extract/*.csv; do
+for file in "$temp_dir"/*.csv; do
     mv "$file" "./data/$curr_date"
 done
 
-# clears temp directory and wget file
-rm -r /tmp/extract
+# cleans up temp directory and wget file
+rm -rf "$temp_dir"
 rm data.zip
 echo "done"
