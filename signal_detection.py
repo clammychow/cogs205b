1675ee3c5d3cfbd5fa3ff0d398385cb44a07cce4
Anusha <anushs5@uci.edu>
2026-04-17 06:08:04 +0000

    add d_prime and criterion methods

--- signal_detection.py
+++ signal_detection.py
@@ -1,3 +1,5 @@
+import scipy.stats
+
 class SignalDetection:
     def __init__(hits, misses, false_alarms, correct_rejections):
         self.hits = hits
@@ -6,3 +8,10 @@ class SignalDetection:
         self.correct_rejections = correct_rejections
     def hit_rate(self):
         return self.hits / (self.hits + self.misses)
+    def false_alarm_rate(self):
+        return self.false_alarms / (self.false_alarms + self.correct_rejections)
+    def d_prime(self):
+        return scipy.stats.norm.ppf(self.hit_rate) - scipy.stats.norm.ppf(self.false_alarm_rate)
+    def criterion(self):
+        return -0.5 * (scipy.stats.norm.ppf(self.hit_rate) + scipy.stats.norm.ppf(self.false_alarm_rate))
+    
