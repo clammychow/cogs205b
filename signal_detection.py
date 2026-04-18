b1476bfacd95bbd83c0c224947087fa075ad8300
Anusha <anushs5@uci.edu>
2026-04-18 05:23:22 +0000

    add operator overloading

--- signal_detection.py
+++ signal_detection.py
@@ -6,6 +6,7 @@ class SignalDetection:
         self.misses = misses
         self.false_alarms = false_alarms
         self.correct_rejections = correct_rejections
+
     def hit_rate(self):
         return self.hits / (self.hits + self.misses)
     def false_alarm_rate(self):
@@ -15,3 +16,18 @@ class SignalDetection:
     def criterion(self):
         return -0.5 * (scipy.stats.norm.ppf(self.hit_rate) + scipy.stats.norm.ppf(self.false_alarm_rate))
     
+    def __add__(self, other):
+        return SignalDetection(self.hits + other.hits, 
+        self.misses + other.misses,
+        self.false_alarms + other.false_alarms, 
+        self.correct_rejections + other.correct_rejections)
+    def __sub__(self, other):
+        return SignalDetection(self.hits - other.hits, 
+        self.misses - other.misses,
+        self.false_alarms - other.false_alarms, 
+        self.correct_rejections - other.correct_rejections)
+    def __mul__(self, factor):
+        return SignalDetection(self.hits * factor,
+        self.misses * factor,
+        self.false_alarms * factor,
+        self.correct_rejections * factor)
