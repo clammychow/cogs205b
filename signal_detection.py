e299fbc3271cfd6e8990e4e3c702c670449760dd
Anusha <anushs5@uci.edu>
2026-04-19 07:11:42 +0000

    add input validation to operator methods

--- signal_detection.py
+++ signal_detection.py
@@ -4,14 +4,15 @@ class SignalDetection:
     def __init__(self, hits, misses, false_alarms, correct_rejections):
         for i in (hits, misses, false_alarms, correct_rejections):
             if not isinstance(i, int) or isinstance(i, bool):
-                raise TypeError("Inputs must be integers")
+                raise TypeError("Outcome counts must be integers")
             elif i < 0:
-                raise ValueError("Inputs cannot be negative")
+                raise ValueError("Outcome counts cannot be negative")
         self.hits = hits
         self.misses = misses
         self.false_alarms = false_alarms
         self.correct_rejections = correct_rejections
 
+    # core methods
     def hit_rate(self):
         return self.hits / (self.hits + self.misses)
     def false_alarm_rate(self):
@@ -21,18 +22,28 @@ class SignalDetection:
     def criterion(self):
         return -0.5 * (scipy.stats.norm.ppf(self.hit_rate) + scipy.stats.norm.ppf(self.false_alarm_rate))
     
+    # operator overloading
     def __add__(self, other):
+        if not isinstance(other, SignalDetection):
+            raise TypeError("Both objects must be of the SignalDetection class")
         return SignalDetection(self.hits + other.hits, 
         self.misses + other.misses,
         self.false_alarms + other.false_alarms, 
         self.correct_rejections + other.correct_rejections)
+
     def __sub__(self, other):
+        if not isinstance(other, SignalDetection):
+            raise TypeError("Both objects must be of the SignalDetection class")
         return SignalDetection(self.hits - other.hits, 
         self.misses - other.misses,
         self.false_alarms - other.false_alarms, 
         self.correct_rejections - other.correct_rejections)
+
     def __mul__(self, factor):
+        if not isinstance(factor, int) or isinstance(factor, bool):
+            raise TypeError("Factor must be an integer")
         return SignalDetection(self.hits * factor,
         self.misses * factor,
         self.false_alarms * factor,
         self.correct_rejections * factor)
+        
