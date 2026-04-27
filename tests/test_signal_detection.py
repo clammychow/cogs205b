c6c04729fbb9e12beebf980cceee90e0a5a52f10
Anusha <anushs5@uci.edu>
2026-04-27 07:06:06 +0000

    add constructor value and ROC plot tests

--- tests/test_signal_detection.py
+++ tests/test_signal_detection.py
@@ -5,8 +5,9 @@ class TestSignalDetection(unittest.TestCase):
     def setUp(self):
         self.sdt_test1 = SignalDetection(1, 1, 1, 1)
         self.sdt_test2 = SignalDetection(0, 0, 0, 0)
+        self.sdt_test3 = SignalDetection(2, 2, 2, 2)
 
-    # Tests for normal cases
+    # normal cases
     def test_hit_rate(self):
         self.assertEqual(self.sdt_test1.hit_rate(), 0.5)
     
@@ -19,7 +20,7 @@ class TestSignalDetection(unittest.TestCase):
     def test_criterion(self):
         self.assertEqual(self.sdt_test1.criterion(), 0)
 
-    # Tests for edge cases: 0 counts
+    # edge cases: 0 counts
     def test_hit_rate_edge_case(self):
         with self.assertRaises(ValueError) as exception_context:
             self.sdt_test2.hit_rate()
@@ -40,8 +41,20 @@ class TestSignalDetection(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.sdt_test2.criterion()
 
+    # invalid constructor values
+    def test_negative_counts(self):
+        with self.assertRaises(ValueError) as exception_context:
+            sdt_testNeg = SignalDetection(-1, 2, 3, 4)
+        self.assertEqual(str(exception_context.exception),
+            "Outcome counts cannot be negative")
 
-    # Tests for operator arguments
+    def test_nonInt_counts(self):
+        with self.assertRaises(TypeError) as exception_context:
+            sdt_testBool = SignalDetection(True, 1, 2, 3)
+        self.assertEqual(str(exception_context.exception),
+            "Outcome counts must be integers")
+
+    # operator arguments
     def test_add_error(self):
         with self.assertRaises(TypeError) as exception_context:
             self.sdt_test1 + 7
@@ -68,11 +81,18 @@ class TestSignalDetection(unittest.TestCase):
         self.assertEqual(str(exception_context.exception),
             "Outcome counts cannot be negative")
 
+    # operator values
     def test_add(self):
         sdt_testAdd = self.sdt_test1 + self.sdt_test2
         # check for mutation
         self.assertEqual(self.sdt_test1.hits, 1)
+        self.assertEqual(self.sdt_test1.misses, 1)
+        self.assertEqual(self.sdt_test1.false_alarms, 1)
+        self.assertEqual(self.sdt_test1.correct_rejections, 1)
         self.assertEqual(self.sdt_test2.hits, 0)
+        self.assertEqual(self.sdt_test2.misses, 0)
+        self.assertEqual(self.sdt_test2.false_alarms, 0)
+        self.assertEqual(self.sdt_test2.correct_rejections, 0)
         # check values
         self.assertEqual(sdt_testAdd.hits, 1)
         self.assertEqual(sdt_testAdd.misses, 1)
@@ -83,7 +103,13 @@ class TestSignalDetection(unittest.TestCase):
         sdt_testSub = self.sdt_test1 - self.sdt_test2
         # check for mutation
         self.assertEqual(self.sdt_test1.hits, 1)
+        self.assertEqual(self.sdt_test1.misses, 1)
+        self.assertEqual(self.sdt_test1.false_alarms, 1)
+        self.assertEqual(self.sdt_test1.correct_rejections, 1)
         self.assertEqual(self.sdt_test2.hits, 0)
+        self.assertEqual(self.sdt_test2.misses, 0)
+        self.assertEqual(self.sdt_test2.false_alarms, 0)
+        self.assertEqual(self.sdt_test2.correct_rejections, 0)
         # check values
         self.assertEqual(sdt_testSub.hits, 1)
         self.assertEqual(sdt_testSub.misses, 1)
@@ -94,17 +120,26 @@ class TestSignalDetection(unittest.TestCase):
         sdt_testMul = self.sdt_test1 * 2
         # check for mutation
         self.assertEqual(self.sdt_test1.hits, 1)
+        self.assertEqual(self.sdt_test1.misses, 1)
+        self.assertEqual(self.sdt_test1.false_alarms, 1)
+        self.assertEqual(self.sdt_test1.correct_rejections, 1)
         # check values
         self.assertEqual(sdt_testMul.hits, 2)
         self.assertEqual(sdt_testMul.misses, 2)
         self.assertEqual(sdt_testMul.false_alarms, 2)
         self.assertEqual(sdt_testMul.correct_rejections, 2)
 
-    def test_ROC(self):
-        with self.assertRaises(ValueError):
-            SignalDetection.plot_roc([self.sdt_test1, self.sdt_test2])
-        # failed test
-        #self.assertEqual(SignalDetection.plot_roc([self.sdt_test1, self.sdt_test2]), fig, ax)
+    # ROC Plot
+    def test_ROC_handles_sequence(self):
+        import matplotlib.figure
+        sdt_list = [self.sdt_test1, self.sdt_test3]
+        fig, ax = SignalDetection.plot_roc(sdt_list)
+        self.assertIsInstance(fig, matplotlib.figure.Figure)
+
+    # failed test
+    #def test_ROC_handles_sequence(self):
+        #sdt_list = [self.sdt_test1, self.sdt_test3]
+        #self.assertEqual(SignalDetection.plot_roc(sdt_list), fig, ax)
 
 if __name__ == '__main__':
     unittest.main()
