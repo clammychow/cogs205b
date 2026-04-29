8815f2f300b9cc5ee1335c8a0927a27c3ddf1033
Anusha <anushs5@uci.edu>
2026-04-29 22:26:14 +0000

    create BayesFactor skeleton

---
+++ bayes_factor/bayes_factor.py
@@ -0,0 +1,13 @@
+
+class BayesFactor:
+    def __init__(n, k):
+        self.n = n
+        self.k = k
+    
+    def likelihood(self, theta):
+
+    def evidence_slab(self):
+
+    def evidence_spike(self):
+
+    def bayes_factor(self):
---
+++ bayes_factor/tests/test_bayes_factor.py
@@ -0,0 +1,7 @@
+import unittest
+from bayes_factor import BayesFactor
+
+class TestBayesFactor(unittest.TestCase):
+
+if __name__ == '__main__':
+    unittest.main()
